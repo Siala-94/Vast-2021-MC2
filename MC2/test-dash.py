572bb256d12f3050b00516de30332f4863899c8a
Hassan Mustafa <sialahassan94@gmail.com>
2023-05-21 18:35:17 +0200

    1 finished

--- MC2/test-dash.py
+++ MC2/test-dash.py
@@ -30,9 +30,14 @@ def read_data(data1, data2):
 
 def getHeatmap(df):
     df_sorted = df.sort_values(by='date')
+
     fig = px.density_heatmap(
-        df_sorted, x="date", y="location", color_continuous_scale='ylorrd')
-    fig.update_layout(xaxis={'range': [df['date'].min(), df['date'].max()]})
+        df_sorted,
+        x="date", y="location",
+        color_continuous_scale='ylorrd',
+        nbinsx=len(df['date'].unique()),
+        nbinsy=len(df['location'].unique()))
+
     return fig
 
 
@@ -59,47 +64,113 @@ app.layout = html.Div([
     ]),
 
     html.Div([
+        # multi select
         html.Div([
-            html.Div([
-                html.H6("choose a location to investigate", style={
-                    'text-align': 'center', 'font-family': 'sans-serif'}),
-                dcc.Dropdown(options=[{'label': loc, 'value': loc}
-                                      for loc in getLocations(df)], id="multi", multi=True)
-            ], style={'width': '50%', 'display': 'inline-block'}),
-
-            html.Div([
-                html.H6("choose a creditcard to investigate", style={
-                    'text-align': 'center', 'font-family': 'sans-serif'}),
-                dcc.Dropdown(options=[{'label': loc, 'value': loc}
-                                      for loc in get4ccnum(df)], id="4cc", multi=True)
-            ], style={'width': '50%', 'display': 'inline-block'})
-        ]),
+            html.H6("choose a location to investigate", style={
+                'text-align': 'center', 'font-family': 'sans-serif'}),
+            dcc.Dropdown(options=[{'label': loc, 'value': loc}
+                                  for loc in getLocations(df)], id="multi", multi=True)
+        ], style={'width': '50%', 'display': 'inline-block'}),
 
         html.Div([
-            dcc.Graph(id="scatterplot"),
-            dcc.Slider(
-                id='day-slider',
-                max=len(getDates(df))-1,
-                min=0,
-                value=len(getDates(df))-1,
-                marks={i: str(date.strftime("%b %d"))
-                       for i, date in enumerate(getDates(df))},
-                step=1
-            )
-        ])
+            html.H6("choose a creditcard to investigate", style={
+                'text-align': 'center', 'font-family': 'sans-serif'}),
+            dcc.Dropdown(options=[{'label': loc, 'value': loc}
+                                  for loc in get4ccnum(df)], id="4cc", multi=True)
+        ], style={'width': '50%', 'display': 'inline-block'})
 
+    ]),
+
+    # radio buttons
+    html.Div([
+        html.Div([
+            html.H6("choose y-axis"),
+            dcc.RadioItems(options=[{'label': 'Time', 'value': 'time'},
+                                    {'label': 'Location', 'value': 'location'}],
+                           id="y-axis",
+                           style={'display': 'inline-block'})
+        ], style={'width': '33%', 'display': 'inline-block'}),
+        html.Div([
+            html.H6("choose x-axis"),
+            dcc.RadioItems(options=[{'label': 'Time', 'value': 'time'},
+                                    {'label': 'Location', 'value': 'location'}],
+                           id="x-axis",
+                           style={'display': 'inline-block'})
+        ], style={'width': '33%', 'display': 'inline-block'}),
+        html.Div([
+            html.H6("choose type of plot"),
+            dcc.RadioItems(options=[{'label': 'Line Plot', 'value': 'line'},
+                                    {'label': 'Scatter Plot', 'value': 'scatter'}],
+                           id="type-plot",
+                           style={'display': 'inline-block'})
+        ], style={'width': '33%', 'display': 'inline-block'})
+    ]),
+
+    # graph and slider
+    html.Div([
+        html.Div([
+            dcc.Graph(id="scatterplot")
+        ], style={'width': '70%', 'display': 'inline-block'}),
+        html.Div([
+            dcc.Graph(id="heatmap")
+        ], style={'width': '30%', 'display': 'inline-block'}),
+        dcc.Slider(
+            id='day-slider',
+            max=len(getDates(df))-1,
+            min=0,
+            value=len(getDates(df))-1,
+            marks={i: str(date.strftime("%b %d"))
+                   for i, date in enumerate(getDates(df))},
+            step=1
+        )
     ])
 
 ])
 
 
 @app.callback(
+    Output('heatmap', 'figure'),
+    Input('day-slider', 'value'),
+    Input('multi', 'value')
+)
+def update_heatmap(date, locations):
+    selected_date = getDates(df)[date]
+
+    df_filtered = df[df['date'] == selected_date]
+    df_filtered = df_filtered[df_filtered['location'].isin(
+        locations)] if locations else df_filtered
+
+    df_count = df_filtered.groupby('location').size().reset_index(name='count')
+    df_sorted = df_count.sort_values(by='count', ascending=True)
+
+    fig = px.bar(
+        df_sorted,
+        x='count',
+        y='location',
+        color='count',
+        color_continuous_scale='YlOrRd',
+        orientation='h'
+    )
+
+    fig.update_layout(
+        yaxis={'title': 'Locations'},
+        xaxis={'title': 'Count'}
+    )
+
+    return fig
+
+
+@app.callback(
     Output('scatterplot', 'figure'),
     Input('day-slider', 'value'),
     Input('multi', 'value'),
-    Input('4cc', 'value')
+    Input('4cc', 'value'),
+    Input('y-axis', 'value'),
+    Input('x-axis', 'value'),
+    Input('type-plot', 'value')
+
 )
-def update_plot(date, locations, num):
+def update_plot(date, locations, num, yaxis, xaxis, plot):
     selected_date = getDates(df)[date]
 
     df_new = df[df['date'] == selected_date]
@@ -109,11 +180,43 @@ def update_plot(date, locations, num):
 
     df_new = df_new[df_new['last4ccnum'].isin(num)] if num else df_new
 
-    fig = px.scatter(df_new, x="time", y="location", color="last4ccnum")
+    # Convert time to datetime data type
+    df_new['time'] = pd.to_datetime(df_new['time'])
+    df_new = df_new.sort_values(by='time')  # Sort by time
+
+    fig = None
+
+    if plot == "line":
+        fig = px.line(
+            df_new,
+            x=xaxis,
+            y=yaxis,
+            text="price",
+            color="last4ccnum",
+            color_discrete_sequence=px.colors.qualitative.Safe,  # Set a fixed color sequence
+            category_orders={xaxis: sorted(df_new[xaxis].unique()),
+                             'last4ccnum': sorted(df_new['last4ccnum'].unique())}  # Set category orders for x-axis and last4ccnum
+        )
+    else:
+        fig = px.scatter(
+            df_new,
+            x=xaxis,
+            y=yaxis,
+            size="price",
+            color="last4ccnum",
+            color_discrete_sequence=px.colors.qualitative.Safe,  # Set a fixed color sequence
+            category_orders={'location': sorted(df_new['location'].unique()),
+                             'last4ccnum': sorted(df_new['last4ccnum'].unique())}  # Set category orders for location and last4ccnum
+        )
+
     fig.update_layout(transition_duration=500)
 
     return fig
 
 
+# ...
+
+
+# ...
 if __name__ == '__main__':
     app.run_server(debug=True)
--- MC2/vast.py
+++ MC2/vast.py
@@ -1,171 +1,60 @@
-# Import necessary libraries
-from dash import Dash, html, dcc, Input, Output
 import pandas as pd
 import plotly.graph_objs as go
-from datetime import datetime
+from dash import Dash, html, dcc, Input, Output
+import geopandas as gpd
 
-# Load data from csv files
-cc_data = pd.read_csv('cc_data.csv', encoding="latin-1")
-loyalty_data = pd.read_csv('loyalty_data.csv', encoding="latin-1")
+# Load GPS data from CSV
+df = pd.read_csv('gps.csv')[:1000]
+df['Timestamp'] = pd.to_datetime(df['Timestamp'])
 
-# Convert timestamp columns to date format
-cc_data['timestamp'] = pd.to_datetime(cc_data['timestamp']).dt.date
-loyalty_data['timestamp'] = pd.to_datetime(loyalty_data['timestamp']).dt.date
+# Load shapefile for geographical mapping
+gdf = gpd.read_file('Geospatial/Kronos_Island.shp')
 
-# Group and aggregate data
-cc_counts = cc_data.groupby(
-    ['location', 'timestamp']).size().reset_index(name='count')
-loyalty_counts = loyalty_data.groupby(
-    ['location', 'timestamp']).size().reset_index(name='count')
-
-# Pivot data for heatmap
-cc_heatmap_data = cc_counts.pivot(
-    index='location', columns='timestamp', values='count')
-loyalty_heatmap_data = loyalty_counts.pivot(
-    index='location', columns='timestamp', values='count')
-
-# Fill missing values with zeros
-cc_heatmap_data = cc_heatmap_data.fillna(0)
-loyalty_heatmap_data = loyalty_heatmap_data.fillna(0)
-
-# Combine data from credit card and loyalty transactions
-combined_heatmap_data = cc_heatmap_data.add(loyalty_heatmap_data, fill_value=0)
-combined_heatmap_data = combined_heatmap_data.fillna(0)
-
-# Create a new Dash application
+# Create Dash application
 app = Dash(__name__)
 
-# Define the layout of the Dash application
 app.layout = html.Div([
-    html.Div([
-        html.H1('Popular Locations and Dates for Credit Card Data'),
-        dcc.Graph(id='heatmap')
-    ], style={'width': '50%', 'display': 'inline-block'}),
-
-    html.Div([
-        dcc.Dropdown(
-            id='location-dropdown',
-            options=[{'label': 'All', 'value': 'all'}] +
-            [{'label': loc, 'value': loc} for loc in cc_heatmap_data.index],
-            value='all'
-        ),
-        dcc.Graph(id='polar-plot'),
-        dcc.Slider(
-            id='day-slider',
-            min=0,
-            max=len(combined_heatmap_data.columns) - 1,
-            value=len(combined_heatmap_data.columns) - 1,
-            marks={i: str(date.strftime("%b %d"))
-                   for i, date in enumerate(combined_heatmap_data.columns)},
-            step=1
-        )
-    ], style={'width': '50%', 'display': 'inline-block', 'vertical-align': 'top'})
+    dcc.Graph(id='gps-movement'),
+    dcc.Interval(
+        id='interval-component',
+        interval=1000,  # Update every second
+        n_intervals=0
+    )
 ])
 
 
-# Define a callback function that updates the heatmap
 @app.callback(
-    Output('heatmap', 'figure'),
-    Input('location-dropdown', 'value')
+    Output('gps-movement', 'figure'),
+    Input('interval-component', 'n_intervals')
 )
-def update_heatmap(location):
-    if location == 'all':
-        data = go.Heatmap(
-            z=combined_heatmap_data.values.tolist(),
-            x=combined_heatmap_data.columns.tolist(),
-            y=combined_heatmap_data.index.tolist(),
-            colorscale='ylorrd'
-        )
-    else:
-        if location in combined_heatmap_data.index:
-            data = go.Heatmap(
-                z=combined_heatmap_data.loc[location].values.tolist(),
-                x=combined_heatmap_data.columns.tolist(),
-                y=[location],
-                colorscale='ylorrd'
-            )
-        else:
-            data = go.Heatmap(
-                z=[[0]],
-                x=[combined_heatmap_data.columns[0]],
-                y=[location],
-                colorscale='ylorrd'
-            )
-    layout = go.Layout(
-        title='Credit Card and Loyalty Transactions',
-        # Increase the font size
-        xaxis={'title': 'Date', 'tickfont': {'size': 10}},
-        yaxis={'title': 'Location', 'tickfont': {
-            'size': 10}}  # Increase the font size
-    )
-    return go.Figure(data=[data], layout=layout)
-
-
-@app.callback(
-    Output('polar-plot', 'figure'),
-    Input('location-dropdown', 'value'),
-    Input('day-slider', 'value')
-)
-def update_polar_plot(location, day):
-    selected_day = combined_heatmap_data.columns[day]
-
-    if location == 'all':
-        data = []
-        for loc in cc_heatmap_data.index:
-            transactions = cc_data[(cc_data['location'] == loc) & (
-                cc_data['timestamp'] == selected_day)].copy()
-            transactions['timestamp'] = pd.to_datetime(
-                transactions['timestamp'])
-            hour_counts = transactions.groupby(
-                transactions['timestamp'].dt.hour).size().reset_index(name='count')
-            angles = [i * 15 for i in hour_counts['timestamp']]
-            hour_counts = hour_counts['count'].tolist()
-            data.append(go.Scatterpolar(
-                r=hour_counts,
-                theta=angles,
-                fill='toself',
-                name=loc,
-                mode='markers+lines',
-                marker=dict(size=5)
-            ))
-    else:
-        if location in cc_heatmap_data.index:
-            transactions = cc_data[(cc_data['location'] == location) & (
-                cc_data['timestamp'] == selected_day)].copy()
-            transactions['timestamp'] = pd.to_datetime(
-                transactions['timestamp'])
-            hour_counts = transactions.groupby(
-                transactions['timestamp'].dt.hour).size().reset_index(name='count')
-            angles = [i * 15 for i in hour_counts['timestamp']]
-            hour_counts = hour_counts['count'].tolist()
-            data = [go.Scatterpolar(
-                r=hour_counts,
-                theta=angles,
-                fill='toself',
-                name=location,
-                mode='markers+lines',
-                marker=dict(size=5)
-            )]
-        else:
-            data = []
-
-    layout = go.Layout(
-        title='Transactions over the Day',
-        polar=dict(
-            radialaxis=dict(
-                tickvals=[0, 10, 20, 30, 40, 50, 60],
-                ticktext=['0', '10', '20', '30', '40', '50', '60']
+def update_gps_movement(n):
+    traces = []
+    for i in range(min(1000, len(df))):
+        trace = go.Scattermapbox(
+            lat=[df['lat'][i]],
+            lon=[df['long'][i]],
+            mode='markers',
+            marker=dict(
+                size=4,
+                color='blue',
+                opacity=0.8
             ),
-            angularaxis=dict(
-                tickmode='array',
-                tickvals=[i * 15 for i in range(24)],
-                ticktext=[str(i) + ':00' for i in range(24)]
-            )
+            name=df['id'][i]
         )
+        traces.append(trace)
+
+    layout = go.Layout(
+        title='GPS Movement',
+        mapbox=dict(
+            style='open-street-map',
+            center=dict(lat=gdf.centroid.y, lon=gdf.centroid.x),
+            zoom=10
+        ),
+        autosize=True
     )
-    return go.Figure(data=data, layout=layout)
+
+    return {'data': traces, 'layout': layout}
 
 
-# Step 7: Run the Dash application
 if __name__ == '__main__':
     app.run_server(debug=True)
